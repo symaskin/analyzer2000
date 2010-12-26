a1010abb4f69d61db7275db44c114f5a704f1d80
Olof Palme <sugkorv@gmail.com>
2010-12-26 16:42:46 +0100

    updated, but broken

--- dict_test.py
+++ dict_test.py
@@ -12,12 +12,21 @@ def emptyFunction(f):
 def file_load(filename):
     return open(filename, 'rU')
 
+def find_nick(nick, column):
+    if nick in column:
+        return True
+
 def aHref(url, nick):
     print '<a href="%s" alt="%s">' % (url, nick)
 
 def main():
-    for line in file_load('testfil'):
-        aHref(line.split()[2], line.split()[1])
+    for line in file_load('utfil'):
+        # only true on first value in column
+        if find_nick('tugg', line.split()[1]):
+            aHref(line.split()[2], line.split()[1])
+        else:
+            print 'nej'
+            break
 
 if __name__ == '__main__':
     main()
