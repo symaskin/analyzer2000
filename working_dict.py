0bffe87d8f34bcfe11ecfab900b918ec3a98d038
Olof Palme <sugkorv@gmail.com>
2010-12-27 18:09:51 +0100

    mu

--- working_dict.py
+++ working_dict.py
@@ -4,12 +4,14 @@ import sys
 import os
 from collections import defaultdict
 
-m = defaultdict(list)
+#nicksAndurls = defaultdict(lambda: defaultdict(list))
+d = defaultdict(list)
 
 f = open('utfil', 'rU')
 
 for lines in f:
-    fields = [(lines.split()[1], lines.split()[2])]
+    fields = [(lines.split()[1], {'file':lines.split()[3], 'url':lines.split()[2], 'date':lines.split()[0]})]
     for k, v in fields:
-        m[k].append(v)
+        d[k.lower()].append(v)
 
+print type(d)
