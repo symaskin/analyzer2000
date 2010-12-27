10a8b4bcd520a11cf6381d02af7b15d42db7e0f5
Olof Palme <sugkorv@gmail.com>
2010-12-27 07:08:56 +0100

    mupp

--- dict_test.py
+++ dict_test.py
@@ -5,6 +5,7 @@ from sys import argv
 import os
 
 program_name = argv[0]
+u = []
 
 def emptyFunction(f):
     pass
@@ -12,13 +13,12 @@ def emptyFunction(f):
 def file_load(filename):
     return open(filename, 'rU')
 
-def nickColumn2list(nick, input):
-    u = []
-    if not nick in input:
-        u.append(nick)
-        print u
-        print nick, 'not in column'
-        raw_input()
+def nickColumn2list():
+    for line in file_load('utfil'):
+        nick = line.split()[1]
+        if not nick in u: 
+            u.append(nick)
+    return u
 
 def find_nick(nick, column):
     if nick in column:
@@ -28,8 +28,8 @@ def aHref(url, nick):
     print '<a href="%s" alt="%s">' % (url, nick)
 
 def main():
-    for line in file_load('utfil'):
-        nickColumn2list('tugg', line.split()[1])
+    nickColumn2list()
+#    for line in file_load('utfil'):
         # only true on first value in column
         #        if find_nick('tugg', line.split()[1]):
             #            aHref(line.split()[2], line.split()[1])
