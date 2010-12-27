fca23f30e8cca138f33b352cbb131325a80519df
Olof Palme <sugkorv@gmail.com>
2010-12-27 09:28:08 +0100

    now picks out all nicks in logfile and puts them in a list.
    next up is to iterate through file for nicks in list and add the urls to a dict with the corresponding nick as key.

--- dict_test.py
+++ dict_test.py
@@ -5,30 +5,10 @@ from sys import argv
 import os
 
 program_name = argv[0]
-u = []
-
+nicks_in_file = []
+nicks_and_url = {}
+i = []
 def emptyFunction(f):
-    pass
-
-def file_load(filename):
-    return open(filename, 'rU')
-
-def nickColumn2list():
-    for line in file_load('utfil'):
-        nick = line.split()[1]
-        if not nick in u: 
-            u.append(nick)
-    return u
-
-def find_nick(nick, column):
-    if nick in column:
-        return True
-
-def aHref(url, nick):
-    print '<a href="%s" alt="%s">' % (url, nick)
-
-def main():
-    nickColumn2list()
 #    for line in file_load('utfil'):
         # only true on first value in column
         #        if find_nick('tugg', line.split()[1]):
@@ -36,6 +16,30 @@ def main():
             #        else:
                 #            print 'nej'
                 #            break
+    pass
+
+def file_load(filename):
+    return open(filename, 'rU')
+    filename.close()
+
+def nickColumn2list():
+    for line in file_load('utfil'):
+        nick = line.split()[1]
+        url = line.split()[2]
+        if not nick in nicks_in_file: 
+            nicks_in_file.append(nick)
+    return nicks_in_file
+
+def find_nick(nick, listan):
+    if nick in listan:
+        return True
+
+def aHref(url, nick):
+    print '<a href="%s" alt="%s">' % (url, nick)
+
+def main():
+    
+    nickColumn2list()
 
 if __name__ == '__main__':
     main()
